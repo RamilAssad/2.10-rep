ff5ba2f81463c10104e558036be81267d16fc0c4
RamilAssad <ramil_tolubaev@mail.ru>
2022-11-27 19:34:21 +0300

    change

--- ind.py
+++ ind.py
@@ -1,7 +1,26 @@
-from functools import reduce
-from operator import mul
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-arr = [1, 0, 2, 3, 0, 4]
-tmp = arr[arr.index(0) + 1:]
-arr = tmp[: arr.index(0) + 1]
-print(sum(arr), reduce(mul, arr), sep='\n')
+def first_last(*args):
+    composition = 1
+    last_arg = args[0]
+    last_ind = 0
+    first_arg = args[0]
+    first_ind = 0
+    for i, item in enumerate(args):
+        if item > last_arg:
+            last_arg = item
+            max_ind = i
+        if item < first_arg:
+            min_arg = item
+            min_ind = i
+    for i in args[first_ind:last_ind]:
+        composition *= i
+    return composition
+
+
+if __name__ == '__main__':
+    arg = list(map(float, input('Введите список аргументов: ').split()))
+    print("Произведение аргументов, расположенных"
+          " между первым и последним аргументами:  ",
+          first_last(*arg))
