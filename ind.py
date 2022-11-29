dc8f2ba97591c6970684d3d11fda6b4a62098d05
RamilAssad <ramil_tolubaev@mail.ru>
2022-11-29 11:44:52 +0300

    fix

--- ind.py
+++ ind.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def first_last(*args):
+
+
+
+def min_max(*args):
+    # Результат
     composition = 1
-    last_arg = args[0]
-    last_ind = 0
-    first_arg = args[0]
-    first_ind = 0
+    # Индекс и item для максимального
+    max_arg = args[0]
+    max_ind = 0
+
+    # Индекс и item для минимального
+    min_arg = args[0]
+    min_ind = 0
+    # Проходит по списку и вычисляет max и min
     for i, item in enumerate(args):
-        if item > last_arg:
-            last_arg = item
+        if item > max_arg:
+            max_arg = item
             max_ind = i
-        if item < first_arg:
+        if item < min_arg:
             min_arg = item
             min_ind = i
-    for i in args[first_ind:last_ind]:
+    for i in args[min_ind:max_ind]:
         composition *= i
     return composition
 
@@ -22,5 +30,7 @@ def first_last(*args):
 if __name__ == '__main__':
     arg = list(map(float, input('Введите список аргументов: ').split()))
     print("Произведение аргументов, расположенных"
-          " между первым и последним аргументами:  ",
-          first_last(*arg))
+          " между максимальным и минимальным аргументами:  ",
+          min_max(*arg))
+    if not arg:
+        print("None")
